335c59514f47e6d00b87745898323cbd7f9c6d6c
sayantanHack <sayantandatta97@gmail.com>
2020-08-05 21:21:03 +0530

    Addindg mycontracts solidity code

---
+++ MyContractUpdate.sol
@@ -0,0 +1,20 @@
+pragma solidity 0.5.1;
+
+contract MyContract {
+    
+    // we can use public keyword to skip the get function 
+    // we can also get rid of the constructor by defining value here only
+    
+    string public value;
+    
+    
+    constructor() public {
+        value = "This is my value";
+    }
+    
+    
+    function set(string memory val) public {
+        value = val;
+    }
+    
+}
